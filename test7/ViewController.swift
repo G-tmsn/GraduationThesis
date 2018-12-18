38e1d23381c296e4b3605b7aa4fcb06fdff7c65e
Kenji Tsukamoto <tenbook485@gmail.com>
2018-12-18 17:30:45 +0900

    節点の座標を取ってそれを元に音楽を流せるようになった

--- test7/ViewController.swift
+++ test7/ViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 import MapKit
 import CoreLocation
+import AVFoundation
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     var myMapView: MKMapView!
     var myLocationManager: CLLocationManager!
+    var audioPlayer: AVAudioPlayer!
     var timer: Timer!
     var count = 0
     
@@ -121,6 +123,10 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     var route: MKRoute!
     
+    var myTarget: CLLocationCoordinate2D!
+    var lastAngle: Int!
+    var nowAngle: Int!
+    
     //======================================================================================================
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -159,6 +165,8 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         // 目的地の緯度、経度を設定.
         requestLatitude = 35.7134
         requestLongitude = 139.7044
+        // requestLatitude = 37.7758
+        // requestLongitude = -122.4264
         
         // 目的地の座標を指定.
         requestCoordinate = CLLocationCoordinate2DMake(requestLatitude, requestLongitude)
@@ -230,7 +238,8 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             var heading: CLHeading? { get {
                 return self.myLocationManager.heading
                 }
-            }/*
+            }
+            /*
             print(heading)
             
             if(self.thereIsRoute == true){
@@ -242,17 +251,37 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
                 
                 self.myMapView.addOverlay(self.route.steps[1].polyline)
             }*/
+            
+            // １つ目の曲がり角のと緯度軽度を取得
+            self.myTarget = self.route.steps[1].polyline.coordinate
+            
+            // 1つ目の曲がり角の角度を取得
+            if(self.thereIsRoute == true){
+                
+                self.lastAngle = self.nowAngle
+                self.nowAngle = self.angle(currentLocation: self.myLocation, targetLocation: self.myTarget)
+                if(self.lastAngle == nil){
+                    self.lastAngle = self.nowAngle
+                }
+                // print(self.lastAngle)
+                // print(self.nowAngle)
+                
+                // 角度が大きく変わっていたら音楽を流す
+                if(self.angleChanged(previousAngle: self.lastAngle, nowAngle: self.nowAngle)){
+                // if(self.lastAngle != self.nowAngle){
+                    print("You are wrong")
+                    self.playSound(name: "sound1")
+                } else {
+                    print("You are right")
+                }
+            }
         }
         
     }
     
-    //======================================================================================================
-    struct Point {
-        var x:Double = 0
-        var y:Double = 0
-    }
+    //==================================================================================================
     
-    //中継メソッド
+    // 中継メソッド
     @objc func misstakeDetecter(timer: Timer) {
         
         count += 1
@@ -263,98 +292,40 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         locationManager(myLocationManager)
     }
     
-    //==================================================================================================
-    
- /*
-    // Regionが変更した時に呼び出されるメソッド
-    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool, didUpdateLocations locations: [CLLocation]) {
-        print("regionDidChangeAnimated")
-        self.myMapView.removeOverlay(toPin as! MKOverlay)
+    // 方角を取得するメソッド
+    func angle(currentLocation: CLLocationCoordinate2D, targetLocation: CLLocationCoordinate2D) -> Int {
         
-        // 既に生成しているルートを削除
-        if(thereIsRoute == true){
-            self.myMapView.removeAnnotation(toPin)
-            thereIsRoute = false
+        let currentLatitude = currentLocation.latitude
+        let currentLongitude = currentLocation.longitude
+        let targetLatitude = targetLocation.latitude
+        let targetLongitude = targetLocation.longitude
+        
+        let difLongitude = targetLongitude - currentLongitude
+        let y = sin(difLongitude)
+        let x = cos(currentLatitude) * tan(targetLatitude) - sin(currentLatitude) * cos(difLongitude)
+        let p = atan2(y, x) * 180 / Double.pi
+        
+        if p < 0 {
+            return Int(360 + atan2(y, x) * 180 / Double.pi)
         }
+        return Int(atan2(y, x) * 180 / Double.pi)
+    }
+    
+    
+    // 角度の差を計算するメソッド
+    func angleChanged(previousAngle: Int, nowAngle: Int) -> Bool {
         
-        // 配列から現在座標を取得.
-        myLocations = locations as NSArray
-        myLastLocation = myLocations.lastObject as? CLLocation
-        myLocation = myLastLocation.coordinate
+        // 2つの角度の差を計算
+        let diff: Int = (previousAngle - nowAngle) % 360
         
-        print("\(myLocation.latitude), \(myLocation.longitude)")
-        
-        // 目的地の緯度、経度を設定.
-        requestLatitude = 35.7134
-        requestLongitude = 139.7044
-        
-        // 目的地の座標を指定.
-        requestCoordinate = CLLocationCoordinate2DMake(requestLatitude, requestLongitude)
-        //let fromCoordinate = CLLocationCoordinate2DMake(myLatitude, myLongitude)
-        fromCoordinate = myLocation
-        
-        // PlaceMarkを生成して出発点、目的地の座標をセット.
-        fromPlace = MKPlacemark(coordinate: fromCoordinate, addressDictionary: nil)
-        toPlace = MKPlacemark(coordinate: requestCoordinate, addressDictionary: nil)
-        
-        
-        // Itemを生成してPlaceMarkをセット.
-        fromItem = MKMapItem(placemark: fromPlace)
-        toItem = MKMapItem(placemark: toPlace)
-        
-        // MKDirectionsRequestを生成.
-        myRequest = MKDirections.Request()
-        
-        // 出発地のItemをセット.
-        myRequest.source = fromItem
-        
-        // 目的地のItemをセット.
-        myRequest.destination = toItem
-        
-        // 複数経路の検索を有効.
-        myRequest.requestsAlternateRoutes = true
-        
-        // 移動手段を徒歩に設定.
-        myRequest.transportType = MKDirectionsTransportType.walking
-        
-        // MKDirectionsを生成してRequestをセット.
-        myDirections = MKDirections(request: myRequest)
-        
-        myDirections.calculate { (response, error) in
-            
-            // NSErrorを受け取ったか、ルートがない場合.
-            if error != nil || response!.routes.isEmpty {
-                return
-            }
-            
-            let route: MKRoute = response!.routes[0] as MKRoute
-            print("目的地までぇ \(route.distance)km")
-            print("所要時間 \(Int(route.expectedTravelTime/60))分")
-            
-            // mapViewにルートを描画.
-            self.myMapView.addOverlay(route.polyline)
-            
-            // ピンを生成.
-            // let fromPin: MKPointAnnotation = MKPointAnnotation()
-            self.toPin = MKPointAnnotation()
-            
-            // 座標をセット.
-            // fromPin.coordinate = fromCoordinate
-            self.toPin.coordinate = self.requestCoordinate
-            
-            // titleをセット.
-            // fromPin.title = "出発地点"
-            self.toPin.title = "目的地"
-            
-            // mapViewに追加.
-            // self.myMapView.addAnnotation(fromPin)
-            self.myMapView.addAnnotation(self.toPin)
-            self.thereIsRoute = true
+        if(diff < 30){
+            return false
+        } else {
+            return true
         }
     }
-    */
-    //======================================================================================================
     
+    //==================================================================================================
     
     
     // 認証が変更された時に呼び出されるメソッド.
@@ -391,5 +362,30 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+}
+
+
+//======================================================================================================
+
+// 音楽再生のための拡張クラス
+extension ViewController: AVAudioPlayerDelegate {
+    func playSound(name: String) {
+        guard let path = Bundle.main.path(forResource: name, ofType: "wav") else {
+            print("音源ファイルが見つかりません")
+            return
+        }
+        
+        do {
+            // AVAudioPlayerのインスタンス化
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            
+            // AVAudioPlayerのデリゲートをセット
+            audioPlayer.delegate = self
+            
+            // 音声の再生
+            audioPlayer.play()
+        } catch {
+        }
     }
 }
